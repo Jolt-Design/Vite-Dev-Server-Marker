a0c878cc360b15540154c0d0a77df01c98bf9a66
Jon Goodger <jonno.is@gmail.com>
2025-08-26 18:01:48 +0100

    Improved cleanup logic

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,33 @@
-import { rm, writeFile } from 'node:fs/promises'
+import { rmSync } from 'node:fs'
+import { writeFile } from 'node:fs/promises'
 import type { PluginOption } from 'vite'
 
 const MARKER_FILE = '.jolt-marker.tmp'
+const EXIT_SIGNALS = [
+	'SIGABRT',
+	'SIGBUS',
+	'SIGFPE',
+	'SIGHUP',
+	'SIGILL',
+	'SIGINT',
+	'SIGQUIT',
+	'SIGSEGV',
+	'SIGTERM',
+	'SIGTRAP',
+	'SIGUSR1',
+	'SIGUSR2',
+]
 
 export default function joltDevServerMarker(): PluginOption {
 	let wroteFile = false
+	let cleanupRegistered = false
+
+	const cleanUpFile = () => {
+		if (wroteFile) {
+			rmSync(MARKER_FILE)
+			wroteFile = false
+		}
+	}
 
 	return {
 		name: 'jolt-dev-server-marker',
@@ -20,11 +43,19 @@ export default function joltDevServerMarker(): PluginOption {
 
 			await writeFile(MARKER_FILE, JSON.stringify(content))
 			wroteFile = true
+
+			if (!cleanupRegistered) {
+				process.on('exit', cleanUpFile)
+
+				for (const signal of EXIT_SIGNALS) {
+					process.on(signal, () => process.exit())
+				}
+
+				cleanupRegistered = true
+			}
 		},
 		async buildEnd(): Promise<void> {
-			if (wroteFile) {
-				await rm(MARKER_FILE)
-			}
+			cleanUpFile()
 		},
 	}
 }
